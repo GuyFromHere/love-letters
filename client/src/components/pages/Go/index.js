7d102956c08edc0206ce1ecd87986ca49269a79a
Dan Dismuke <dandismuke@gmail.com>
2020-04-28 21:29:45 -0700

    Set up test page with AWS url

--- client/src/components/pages/Go/index.js
+++ client/src/components/pages/Go/index.js
@@ -1,11 +1,51 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Image from '../../partials/Image';
+import { Input, TextArea, FormBtn } from '../../partials/Form';
+import API from '../../../utils/api';
+
+/* 
+TODO:
+- set unique user / capture key
+- get image capture 
+- store reference to capture in DB   
+- upload capture to AWS using key for name
+- call loadImage and get key from database
+- BUT WAIT...will this work or does the key have to be unique every time?
+
+*/
 
 export default function Go() {
+	const [image, setImage] = useState({url: "1_image.png"});
+	const [captureKey, setCaptureKey] = useState();
+	const urlRoot = "https://love-letters-gfh.s3-us-west-2.amazonaws.com/publicprefix/";
+
+	function handleFormSubmit(event) {
+		event.preventDefault();
+		API.saveImageTest({
+			url: "1_image.png"
+		})
+			/* .then(res => loadImage())
+			.catch(err => console.log(err)); */
+	}
+
+	function loadImage() {
+		API.getImage().then(res => {
+			setImage({url: res.data});
+		})
+
+	}
+
+	useEffect(() => {
+		//loadImage();
+	}, [])
+
 	return (
 		<div className="container">
 			<h1>This is the Go page.</h1>
-			<p>Pick a place on the map or enter a location to visit. </p>
-			<p>When you go somewhere you can view letters left by others or leave your own.</p>
+			<h1>{image.url}</h1>
+			<Image src={urlRoot + image.url}/>
+			<Input />
+			<FormBtn />
 		</div>
 	);
 }
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -5,16 +5,21 @@ export default {
 	saveImage: function (imageData, captureInfo) {
 		// Remove header from data and keep the string
 		let base64Image = imageData.split(";base64,").pop();
-		console.log("api saveImage base64Image");
-		//console.log(base64Image);
-		console.log(captureInfo);
+
 		// Send to server as string and convert it to an image
 		axios
 			.post("/api/images/save", { imageData: base64Image, captureInfo: captureInfo })
-			.then((result) => {
+/* 			.then((result) => {
 				// send image info back to browser
 				console.log("api saveImage result");
 				console.log(result);
-			});
+			}); */
 	},
+	saveImageTest: function () {
+		console.log('api saveimagetest');
+	},
+	getImage: function (key) {
+		console.log('api getImage');
+		axios.get("/api/images/get/" + key)
+	}
 };
