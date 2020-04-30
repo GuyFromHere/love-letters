430b365429b2eb2f89aed4e1e164ebf9c2b15985
GuyFromHere <47902660+GuyFromHere@users.noreply.github.com>
2020-04-30 12:04:59 -0700

    Styles on capture page

--- client/src/components/partials/CaptureImg/index.js
+++ client/src/components/partials/CaptureImg/index.js
@@ -5,8 +5,10 @@ export default function CaptureImg(props) {
 	return (
 		<div className="captureContainer">
 			<img className="capturedImage" src={props.src} alt="Captured image"></img>
-			<i class="fas fa-cloud-upload-alt fa-3x" id="uploadBtn" onClick={props.upload}></i>
-			<i class="fas fa-ban fa-3x" id="resetBtn" onClick={props.reset}></i>
+			<div className="imageIcons">
+				<i class="fas fa-cloud-upload-alt fa-3x" onClick={props.upload}></i>
+				<i class="fas fa-ban fa-3x" onClick={props.reset}></i>
+			</div>
 		</div>
 	);
 }
