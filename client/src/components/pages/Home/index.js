f1214d5c4e5e907a6407480d9e43e5c14bc83ef5
GuyFromHere <47902660+GuyFromHere@users.noreply.github.com>
2020-05-07 13:08:33 -0700

    Added basic modal to letter form. Unable to add markers dynamically after form submits but they are loaded from the DB when the map renders

--- client/src/components/pages/Home/index.js
+++ client/src/components/pages/Home/index.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react";
 import API from "../../../utils/api";
+import Modal from "../../partials/Modal";
 import "./style.css";
 
 //	TODO:
-//	Store marker types and coordinates in DB
+//	Store marker types and location in DB
 // 	When page loads, get marker info and load it on to the map
 // 	When a marker is added, post it to the DB then reload the page centered on the current location.
 
 export default function Home() {
-	const [coordinates, setCoordinates] = useState({});
+	const [location, setLocation] = useState({ lat: "", lng: "" });
+	const [showModal, setShowModal] = useState(false);
 	const googleMapRef = React.createRef();
+	let map;
 
 	useEffect(() => {
 		loadCurrentLocation();
@@ -19,13 +22,15 @@ export default function Home() {
 		// Set default map to current location
 		navigator.geolocation.getCurrentPosition(function (position) {
 			createGoogleMap(position.coords.latitude, position.coords.longitude);
-			setCoordinates({ lat: position.coords.latitude, lng: position.coords.longitude });
+			setLocation({ lat: position.coords.latitude, lng: position.coords.longitude });
 		});
 	};
 
-	// Function to draw marker based on db values for type, id, and coordinates
+	// Function to draw marker based on db values for type, id, and location
 	const drawMarker = (marker, map) => {
-		// get type, id, and coordinates
+		console.log("home drawmarker");
+		console.log(map);
+		// get type, id, and location
 		const markerUri = "https://love-letters-gfh.s3-us-west-2.amazonaws.com/markers/";
 		const icon = {
 			url: markerUri + marker.type + ".png",
@@ -38,29 +43,65 @@ export default function Home() {
 		});
 	};
 
-	const addMarker = (location) => {
-		
-		let locationString = location.toString().replace(")","").replace("(","").split(", ");;
+	const newLetter = (lat, lng, text, map) => {
+		// test text for testing database
+		const newObj = {
+			type: "letter",
+			text: text,
+			lat: lat,
+			lng: lng,
+		};
+
+		API.sendLetter(newObj).then((result) => {
+			console.log("Letter saved to DB.");
+			// Draw the new marker on the map and pan
+			drawMarker(newObj, map);
+			// panTo not working...
+			const position = new window.google.maps.LatLng(lat, lng);
+			console.log(position);
+			//map.panTo(position);
+		});
+		setShowModal(false);
+	};
+
+	/* const addMarker = (location, map) => {
+		// Parse location info from click event so we can save the location in the DB
+		let locationString = location.toString().replace(")", "").replace("(", "").split(", ");
+		const position = new window.google.maps.LatLng(locationString[0], locationString[1]);
+
+		// Call function to create letter here...
+
+		// test text for testing database
 		const newObj = {
 			type: "letter",
 			text: "This is a test.",
 			lat: locationString[0],
 			lng: locationString[1],
-		}
-		API.sendLetter(newObj).then(result => {
-			console.log('letter sent. reload map.');
-			console.log(locationString[0])
-			console.log(locationString[1])
-			createGoogleMap(45.434928402922765, -122.64173556347656);
-			//loadCurrentLocation();
-		})
-	}
+		};
+
+		API.sendLetter(newObj).then((result) => {
+			console.log("Letter saved to DB.");
+		});
+
+		// Draw the new marker on the map and pan
+		drawMarker(newObj, map);
+		map.panTo(position);
+	}; */
+
+	const handleClick = (location, map) => {
+		//
+		let locationString = location.toString().replace(")", "").replace("(", "").split(", ");
+		setShowModal(true);
+		setLocation({ lat: locationString[0], lng: locationString[1] });
+		document.getElementById("leaveLetter").showModal();
+	};
+
+	const closeModal = () => {
+		setShowModal(false);
+	};
 
 	const createGoogleMap = (lat, lng) => {
-
-		// Take a look at this:
-		// https://www.newline.co/fullstack-react/articles/how-to-write-a-google-maps-react-component/#
-		const map = new window.google.maps.Map(googleMapRef.current, {
+		map = new window.google.maps.Map(googleMapRef.current, {
 			zoom: 16,
 			center: { lat: lat, lng: lng },
 			disableDefaultUI: true,
@@ -68,37 +109,30 @@ export default function Home() {
 
 		map.addListener("click", function (mapsMouseEvent) {
 			// call addMarker to handle event
-			addMarker(mapsMouseEvent.latLng)
+			//addMarker(mapsMouseEvent.latLng, map);
+			handleClick(mapsMouseEvent.latLng, map);
 		});
 
+		// Get letters from DB and draw them on the map after it loads
 		API.getMarkers().then((result) => {
-			console.log('home getMarkers')
 			result.data.forEach((item) => {
-				console.log('home getMarkers result data')
 				drawMarker(item, map);
 			});
 		});
-		
-		
-		/* API.getMarkers().then((result) => {
-			const map = new window.google.maps.Map(googleMapRef.current, {
-				zoom: 16,
-				center: { lat: lat, lng: lng },
-				disableDefaultUI: true,
-			});
-	
-			map.addListener("click", function (mapsMouseEvent) {
-				// call addMarker to handle event
-				addMarker(mapsMouseEvent.latLng)
-
-			});
-
-			result.data.forEach((item) => {
-				console.log('home getMarkers result data')
-				drawMarker(item, map);
-			});
-		}); */ 
 	};
 
-	return <div id="google-map" ref={googleMapRef} />;
+	return (
+		<div>
+			<div id="google-map" ref={googleMapRef}></div>
+			{showModal ? (
+				<Modal
+					id="leaveLetter"
+					className="modal"
+					send={newLetter}
+					close={closeModal}
+					location={location}
+				/>
+			) : null}
+		</div>
+	);
 }
---
+++ client/src/components/partials/Modal/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./style.css";
+
+export default function Modal(props) {
+	const handleClick = () => {
+		const text = document.getElementById("letterText").value;
+		props.send(props.location.lat, props.location.lng, text);
+	};
+
+	return (
+		<dialog id={props.id} className="modalContainer">
+			<div className="modalHeader">
+				<h2>Type your message:</h2>
+			</div>
+			<div className="modalBody">
+				{props.location.lat} {props.location.lng}
+				<textarea id="letterText"></textarea>
+			</div>
+			<div className="modalFooter">
+				<button className="btnCancel" onClick={props.close}>
+					CLOSE
+				</button>
+				<button className="btnSubmit" onClick={handleClick}>
+					SEND
+				</button>
+			</div>
+		</dialog>
+	);
+}
